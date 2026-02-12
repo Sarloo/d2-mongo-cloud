ddc0faa575a1fb6cba14dee6bfe50016004e2f79
Sarloo <carloscecor05@gmail.com>
2026-02-12 13:46:06 -0600

    inicializar repositorio

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello, MongoDB!');
+});
+
+// start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
